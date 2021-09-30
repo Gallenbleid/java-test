2e528cc2276f746f882b7eba2f73dc04a70aa561
Gallenbleid <50660293+Gallenbleid@users.noreply.github.com>
2021-09-30 12:59:46 +0200

    Add files via upload

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -5,35 +5,90 @@
  */
 package javaapplication1;
 
-import com.sun.javafx.scene.layout.region.Margins;
-import com.sun.xml.internal.bind.v2.runtime.RuntimeUtil;
-import com.sun.xml.internal.ws.commons.xmlutil.Converter;
+//import com.sun.javafx.scene.layout.region.Margins;
+//import com.sun.xml.internal.bind.v2.runtime.RuntimeUtil;
+//import com.sun.xml.internal.ws.commons.xmlutil.Converter;
 import java.util.ArrayList;
-import java.util.Random;
 import monsters.Monster;
-import monsters.Skeleton;
-import monsters.Zombie;
-import monsters.Enderman;
 import monsters.MonsterGenerator;
+import java.util.Random;
+import java.util.Scanner;
+import java.awt.event.KeyEvent;
+import java.util.stream.IntStream;
         
 /**
  *
  * @author jimy
  */
 public class JavaApplication1 {
+    
+    static ArrayList<Monster> monsterList = new ArrayList<>();
+    static Random rd = new Random();
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-               
-        ArrayList<Monster> monsterList = new ArrayList<>();
+    public static void main(String[] args) {       
+        
+        Scanner kscan = new Scanner(System.in);
+        Boolean wybor = true;
+        Monster agresor, victim;
+        
+        
         MonsterGenerator.getMonsters(5, monsterList);
-        MonsterGenerator.printAllMonsters(monsterList);        
-        monsterList.get(0).atack(monsterList.get(1));
+        System.out.println("-------------Zaczynamy igrzyska śmierci---------");
+        String klawisz = "t";        
+        do{                        
+            if (klawisz.equals("t")) {
+                agresor = JavaApplication1.getAgresor();
+                victim = JavaApplication1.getVictim();
+                System.out.println("Przygotowanie do walki");
+                System.out.println(".....");
+                //System.out.println("Wybrano Agresora: "+agresor);
+                //System.out.println("Poziom agresora: " + agresor.getLevel());
+                //System.out.println("Wybrano Ofiarę: "+victim);
+                if (victim != agresor){                    
+                    JavaApplication1.walka(agresor, victim);
+                }else            
+                    System.out.println("Nie walczę ze sobą");
+                    //MonsterGenerator.printAllMonsters(monsterList);
+                //
+            }
+            else {
+                wybor =false;
+                System.out.println("Po walkach.");
+                break;
+            }
+            System.out.println("Kolejna walka?");
+            klawisz = kscan.next();
+        }while(wybor);
+        
+    } 
+    public static Monster getAgresor(){
+        Monster a = monsterList.get(rd.nextInt(monsterList.size()));
+        System.out.print("Wybrano Atakującego : " + a.getName() + " --> ");
+        return a;
+    }
+    public static Monster getVictim(){
+        Monster v = monsterList.get(rd.nextInt(monsterList.size()));
+        System.out.println("Wybrano Ofiarę : " + v.getName());
+        return v;
+    }
+    public static void walka(Monster agresor, Monster victim) {        
+        agresor.badLook();
+        agresor.atack(victim);
+        double vPd = victim.getPD();
+        int aLevel = agresor.getLevel();
+        System.out.println("Stan ofiary " + vPd);
+        if (vPd <= 0){
+            System.out.println(victim.getName() + " zdechł!");
+            monsterList.remove(victim);            
+            aLevel++;
+            agresor.setLevel(aLevel);
+            System.out.println(agresor.getLevel());
+        }
+        System.out.println("-------------Stan potworów po walce.-------------");
         MonsterGenerator.printAllMonsters(monsterList);
-        Object annownObject=monsterList.get(1).getClass();
-        System.out.println(annownObject); //popracuj nad pobraniem rodzaju klasy do metod!!!!
-    }    
+    }
     
 }
